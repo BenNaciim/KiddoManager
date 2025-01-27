e07b5157cb65adb84ada7f4afd15e70888bc0df2
Nacim Benali <benali.nacim@gmail.com>
2025-01-27 21:41:46 +0100

    init KiddoManager Project

---
+++ src/test/java/kiddo/kiddomanager/KiddoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package kiddo.kiddomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiddoManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
