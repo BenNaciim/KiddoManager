a2e1feb5c15f7f7b2e659972de8c971ce825f98f
Nacim Benali <benali.nacim@gmail.com>
2025-02-03 17:14:39 +0100

    add Registration controller

--- src/main/java/kiddo/kiddomanager/controllers/RegistrationController.java
+++ src/main/java/kiddo/kiddomanager/controllers/RegistrationController.java
@@ -3,11 +3,14 @@ package kiddo.kiddomanager.controllers;
 import kiddo.kiddomanager.models.Parents;
 import kiddo.kiddomanager.models.Personal;
 import kiddo.kiddomanager.services.RegistrationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/register")
@@ -20,13 +23,12 @@ public class RegistrationController {
     }
 
     @PostMapping("/parents")
-    public ResponseEntity<String> registerParents(@RequestBody Parents parents) {
-        registrationService.registerParents(parents);
-        return ResponseEntity.ok("Parents registered successfully");
+    public ResponseEntity<HttpStatus> registerParents(@RequestBody Parents parents) {
+        return Objects.nonNull(registrationService.registerParents(parents))? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
     }
+
     @PostMapping("/employee")
-    public ResponseEntity<String> registerEmploye(@RequestBody Personal personal) {
-        registrationService.registerEmployee(personal);
-        return ResponseEntity.ok("Personal registered successfully");
+    public ResponseEntity<HttpStatus> registerEmployee(@RequestBody Personal personal) {
+        return Objects.nonNull(registrationService.registerEmployee(personal))? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
     }
 }
